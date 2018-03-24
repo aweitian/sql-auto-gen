5bc4d3f87f135aa0973d944c5e3066b1d075cf0e
tiananwei <awei.tian@qq.com>
2018-03-24 17:59:45 +0800

    test unit

---
+++ src/Mysql/gen.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/24
+ * Time: 17:11
+ */
+
+namespace Aw\Sql\Mysql;
+
+use Aw\Build\Mysql\Crud;
+use Aw\Cache\ICache;
+use Aw\Db\Connection\Mysql;
+use Aw\Db\Reflection\Mysql\Table;
+
+
+class gen
+
+{
+    /**
+     * @var ICache
+     */
+    protected $cache = null;
+
+    /**
+     * @var Mysql
+     */
+    protected $con;
+    /**
+     * @var string
+     */
+    protected $table;
+
+    public function setConnection(Mysql $con)
+    {
+        $this->con = $con;
+        return $this;
+    }
+
+    public function setTableName($name)
+    {
+        $this->table = $name;
+        return $this;
+    }
+
+    public function setCache(ICache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function getColumns()
+    {
+        $db_ref = new Table($this->table, $this->con, $this->cache);;
+        return $db_ref->getColumnNames();
+    }
+
+    public function select($where = '', $fields = '')
+    {
+        if (is_string($where)) {
+            if ($where)
+                $where = explode(',', $where);
+            else
+                $where = array();
+        } else {
+            $where = (array)$where;
+        }
+        $build = new Crud($this->table);
+        if (is_string($fields)) {
+            if ($fields)
+                $fields = explode(',', $fields);
+            else
+                $fields = array();
+        } else {
+            $fields = (array)$fields;
+        }
+        foreach ($fields as $field) {
+            $build->bindField($field);
+        }
+        foreach ($where as $item) {
+            $build->bindWhere($item);
+        }
+        return $build->select();
+    }
+
+    public function insert($fields = '')
+    {
+        $db_ref = new Table($this->table, $this->con, $this->cache);;
+        $build = new Crud($this->table);
+        if (is_string($fields)) {
+            if ($fields)
+                $fields = explode(',', $fields);
+            else
+                $fields = $db_ref->getColumnNames();
+        } else {
+            $fields = (array)$fields;
+        }
+        foreach ($fields as $field) {
+            if ($db_ref->isPk($field))
+                continue;
+            $build->bindField($field);
+        }
+        return $build->insert();
+    }
+
+    public function remove()
+    {
+        $db_ref = new Table($this->table, $this->con, $this->cache);;
+        $build = new Crud($this->table);
+        $pks = $db_ref->getPk();
+        foreach ($pks as $pk) {
+            $build->bindWhere($pk);
+        }
+        return $build->delete();
+    }
+
+    public function update($fields = '')
+    {
+        $db_ref = new Table($this->table, $this->con, $this->cache);;
+        $build = new Crud($this->table);
+        if (is_string($fields)) {
+            if ($fields)
+                $fields = explode(',', $fields);
+            else
+                $fields = $db_ref->getColumnNames();
+        } else {
+            $fields = (array)$fields;
+        }
+        foreach ($fields as $field) {
+            if ($db_ref->isPk($field)) {
+                $build->bindWhere($field);
+            } else {
+                $build->bindField($field);
+            }
+
+        }
+        return $build->update();
+    }
+}
---
+++ tests/BaseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class BaseTest extends PHPUnit_Framework_TestCase
+{
+    public function testCommon()
+    {
+        $gen = new \Aw\Sql\Mysql\gen();
+        $gen->setConnection(new Aw\Db\Connection\Mysql (array(
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'database' => 'hdf'
+        )));
+        $gen->setTableName('data_article');
+        $this->assertEquals("SELECT * FROM data_article", $gen->select());
+        $this->assertEquals("INSERT INTO data_article (kw,desc,thumb,title,content,date) VALUES (:kw,:desc,:thumb,:title,:content,:date)", $gen->insert());
+        $this->assertEquals("DELETE FROM data_article WHERE sid = :sid", $gen->remove());
+        $this->assertEquals("UPDATE data_article SET kw=:kw,desc=:desc,thumb=:thumb,title=:title,content=:content,date=:date WHERE sid = :sid", $gen->update());
+    }
+
+    public function testWhere()
+    {
+        $gen = new \Aw\Sql\Mysql\gen();
+        $gen->setConnection(new Aw\Db\Connection\Mysql (array(
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'database' => 'hdf'
+        )));
+        $gen->setTableName('data_article');
+        $this->assertEquals("SELECT * FROM data_article WHERE thumb = :thumb AND title = :title", $gen->select('thumb,title'));
+        $this->assertEquals("SELECT kw,desc FROM data_article WHERE thumb = :thumb AND title = :title", $gen->select('thumb,title','kw,desc'));
+        $this->assertEquals("INSERT INTO data_article (kw,desc,thumb,title,content,date) VALUES (:kw,:desc,:thumb,:title,:content,:date)", $gen->insert());
+        $this->assertEquals("DELETE FROM data_article WHERE sid = :sid", $gen->remove());
+        $this->assertEquals("UPDATE data_article SET kw=:kw,desc=:desc,thumb=:thumb,title=:title,content=:content,date=:date WHERE sid = :sid", $gen->update());
+    }
+}
+
